700e892460f497344b872750051a33f763034c3e
darkit-prog <darya.kitaina@gmail.com>
2025-12-10 21:11:14 +0300

    add api, service, schema, types for notifications

--- src/data/schemas/index.schema.ts
+++ src/data/schemas/index.schema.ts
@@ -1,2 +1,3 @@
 export { loginSchema, loginErrorSchema } from "./login.schema";
 export { logoutSchema, logoutUnauthorizedSchema, logoutServerErrorSchema } from "./logout.schema";
+export { obligatoryFieldsSchema, obligatoryRequredFields, schemaErrorsFields, errorSchema } from "./core.schema";
