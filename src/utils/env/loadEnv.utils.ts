c255decda23ccf245203491ef451b62f4cc43899
EphimSh <ephimizm@gmail.com>
2025-12-13 13:54:26 +0300

    fix loadEnv

--- src/utils/env/loadEnv.utils.ts
+++ src/utils/env/loadEnv.utils.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 
 export function loadEnv() {
-  dotenv.config({ path: ".env" });
+  if (process.env.CI) return;
+  dotenv.config({ path: ".env", override: false });
   const env = process.env.ENV ?? "local";
   dotenv.config({ path: `.env.${env}`, override: false });
 }
