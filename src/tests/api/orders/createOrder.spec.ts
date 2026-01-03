41e30702bd63371e669d9642db202674ed20695d
EphimSh <ephimizm@gmail.com>
2026-01-04 00:51:57 +0300

    fix: remove duplicates

--- src/tests/api/orders/createOrder.spec.ts
+++ src/tests/api/orders/createOrder.spec.ts
@@ -21,53 +21,6 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Positive checks]", () => 
     if (orderID) await ordersApiService.deleteOrderWithCustomerAndProduct(orderID, token);
   });
 
-  test("SC-054: Successful order creation", async ({
-    ordersApi,
-    customerApiService,
-    productsApiService,
-    ordersApiService,
-  }) => {
-    productData = generateProductData();
-    const createdProduct = await productsApiService.create(token, productData);
-    const productID = createdProduct._id;
-    customerData = generateCustomerData();
-    const customer = await customerApiService.create(token, customerData);
-    const customerID = customer._id;
-    const orderData = { customer: customerID, products: [productID] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    await ordersApiService.assertOrderStatus(orderResponse, ORDER_STATUSES.DRAFT);
-    expect(orderResponse.body.Order.delivery).toBeNull();
-  });
-  test("SC-055: Create order with multiple products", async ({ ordersApiService }) => {
-    const orderWithDelivery = await ordersApiService.createDraftOrderWithDelivery(token, 3);
-    orderID = orderWithDelivery._id;
-    const totalPriceFromResponse = orderWithDelivery.total_price;
-    const totalPriceCounted = ordersApiService.calculateProductsTotalPrice(orderWithDelivery.products);
-    expect(totalPriceCounted).toEqual(totalPriceFromResponse);
-  });
-  test("SC-108: Successful delivery data creation", async ({ ordersApiService, ordersApi }) => {
-    const orderResponse = await ordersApiService.createDraftOrder(token);
-    orderID = orderResponse._id;
-    const delivery = generateDeliveryData();
-    const response = await ordersApi.updateDeliveryDetails(orderID, delivery, token);
-    await ordersApiService.assertOrderStatus(response, ORDER_STATUSES.DRAFT);
-    expect(response.body.Order.delivery).not.toBeNull();
-  });
-  test("SC-101: Successful status update Draft → In Process", async ({ ordersApiService, ordersApi }) => {
-    const orderWithDelivery = await ordersApiService.createDraftOrderWithDelivery(token);
-    orderID = orderWithDelivery._id;
-    const updateToInProcess = await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.IN_PROCESS }, token);
-    await ordersApiService.assertOrderStatus(updateToInProcess, ORDER_STATUSES.IN_PROCESS);
-  });
-  test("SC-102: Status update In Process → Canceled", async ({ ordersApiService, ordersApi }) => {
-    const orderWithDelivery = await ordersApiService.createDraftOrderWithDelivery(token);
-    orderID = orderWithDelivery._id;
-    const updateToInProcess = await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.IN_PROCESS }, token);
-    await ordersApiService.assertOrderStatus(updateToInProcess, ORDER_STATUSES.IN_PROCESS);
-    const updateToCancelled = await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.CANCELED }, token);
-    await ordersApiService.assertOrderStatus(updateToCancelled, ORDER_STATUSES.CANCELED);
-  });
   test(
     "SC-054: Successful order creation",
     {
@@ -173,103 +126,6 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
     productIds = [];
   });
 
-  test("SC-056: Missing required field customer", async ({ ordersApi, productsApiService }) => {
-    const productData = generateProductData();
-    const createdProduct = await productsApiService.create(token, productData);
-    const productID = createdProduct._id;
-    productIds.push(productID);
-    const orderData = { customer: "", products: [productID] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.NOT_FOUND,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: "Missing customer",
-    });
-  });
-  test("SC-057: Missing required field products", async ({ ordersApi, customerApiService }) => {
-    const customerData = generateCustomerData();
-    const customer = await customerApiService.create(token, customerData);
-    const customerID = customer._id;
-    customerIds.push(customerID);
-    const orderData = { customer: customerID, products: [""] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.SERVER_ERROR,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: "Id was not provided",
-    });
-  });
-  test("SC-058: Empty products arrays", async ({ ordersApi, customerApiService }) => {
-    const customerData = generateCustomerData();
-    const customer = await customerApiService.create(token, customerData);
-    const customerID = customer._id;
-    customerIds.push(customerID);
-    const orderData = { customer: customerID, products: [] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.BAD_REQUEST,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: "Incorrect request body",
-    });
-  });
-  test("SC-059: Non-existent customer ID", async ({ ordersApi, productsApiService }) => {
-    const customerID = generateCustomerID();
-    const productData = generateProductData();
-    const createdProduct = await productsApiService.create(token, productData);
-    const productID = createdProduct._id;
-    productIds.push(productID);
-    const orderData = { customer: customerID, products: [productID] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.NOT_FOUND,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: `Customer with id '${customerID}' wasn't found`,
-    });
-  });
-  test("SC-060: Invalid customer ID format", async ({ ordersApi, productsApiService }) => {
-    const customerID = "invalid_id";
-    const productData = generateProductData();
-    const createdProduct = await productsApiService.create(token, productData);
-    const productID = createdProduct._id;
-    productIds.push(productID);
-    const orderData = { customer: customerID, products: [productID] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.SERVER_ERROR,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: 'Cast to ObjectId failed for value "invalid_id" (type string) at path "_id" for model "Customer"',
-    });
-  });
-  test("SC-061: Non-existent product ID in array", async ({ ordersApi, customerApiService }) => {
-    const productID = generateProductID();
-    const customerData = generateCustomerData();
-    const customer = await customerApiService.create(token, customerData);
-    const customerID = customer._id;
-    customerIds.push(customerID);
-    const orderData = { customer: customerID, products: [productID] };
-    const orderResponse = await ordersApi.create(orderData, token);
-    validateResponse(orderResponse, {
-      status: STATUS_CODES.NOT_FOUND,
-      schema: errorSchema,
-      IsSuccess: false,
-      ErrorMessage: `Product with id '${productID}' wasn't found`,
-    });
-  });
-  test("SC-062: Check delivery field in response", async ({ ordersApiService }) => {
-    const orderResponse = await ordersApiService.createDraftOrder(token);
-    orderID = orderResponse._id;
-    expect(orderResponse.delivery).toBeNull();
-  });
-  test("SC-063: Check order history creation", async ({ ordersApiService }) => {
-    const orderResponse = await ordersApiService.createDraftOrder(token);
-    orderID = orderResponse._id;
-    expect(orderResponse.history[0]?.action).toBe(ORDER_HISTORY_ACTIONS.CREATED);
-  });
   test(
     "SC-056: Missing required field customer",
     {
@@ -279,6 +135,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const productData = generateProductData();
       const createdProduct = await productsApiService.create(token, productData);
       const productID = createdProduct._id;
+      productIds.push(productID);
       const orderData = { customer: "", products: [productID] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -298,6 +155,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const customerData = generateCustomerData();
       const customer = await customerApiService.create(token, customerData);
       const customerID = customer._id;
+      customerIds.push(customerID);
       const orderData = { customer: customerID, products: [""] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -317,6 +175,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const customerData = generateCustomerData();
       const customer = await customerApiService.create(token, customerData);
       const customerID = customer._id;
+      customerIds.push(customerID);
       const orderData = { customer: customerID, products: [] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -337,6 +196,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const productData = generateProductData();
       const createdProduct = await productsApiService.create(token, productData);
       const productID = createdProduct._id;
+      productIds.push(productID);
       const orderData = { customer: customerID, products: [productID] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -357,6 +217,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const productData = generateProductData();
       const createdProduct = await productsApiService.create(token, productData);
       const productID = createdProduct._id;
+      productIds.push(productID);
       const orderData = { customer: customerID, products: [productID] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -377,6 +238,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
       const customerData = generateCustomerData();
       const customer = await customerApiService.create(token, customerData);
       const customerID = customer._id;
+      customerIds.push(customerID);
       const orderData = { customer: customerID, products: [productID] };
       const orderResponse = await ordersApi.create(orderData, token);
       validateResponse(orderResponse, {
@@ -394,6 +256,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
     },
     async ({ ordersApiService }) => {
       const orderResponse = await ordersApiService.createDraftOrder(token);
+      orderID = orderResponse._id;
       expect(orderResponse.delivery).toBeNull();
     },
   );
@@ -404,6 +267,7 @@ test.describe("[API] [Sales Portal] [Orders] [Create - Negative checks]", () => 
     },
     async ({ ordersApiService }) => {
       const orderResponse = await ordersApiService.createDraftOrder(token);
+      orderID = orderResponse._id;
       expect(orderResponse.history[0]?.action).toBe(ORDER_HISTORY_ACTIONS.CREATED);
     },
   );
--- src/tests/api/orders/updateOrder.spec.ts
+++ src/tests/api/orders/updateOrder.spec.ts
@@ -1,7 +1,7 @@
 import { generateCustomerData } from "data/customers/generateCustomerData";
 import { generateID } from "data/generateID";
 import { generateDeliveryData } from "data/orders/generateDeliveryData";
-import { generateProductData } from "data/products/generateProductData";
+import { generateProductData, listOfProductsId } from "data/products/generateProductData";
 import { errorSchema } from "data/schemas/core.schema";
 import { createOrderSchema } from "data/schemas/orders/create.schema";
 import { STATUS_CODES } from "data/statusCode";
@@ -9,6 +9,7 @@ import { IOrderCreateBody, ORDER_STATUSES } from "data/types/orders.types";
 // import { IProductFromResponse } from "data/types/products.types";
 import { TEST_TAG, COMPONENT_TAG } from "data/types/tags.types";
 import { expect, test } from "fixtures/api.fixtures";
+import { getRandomElement, getRandomElements } from "utils/random.utils";
 import { validateResponse } from "utils/validation/validateResponse.utils";
 
 test.describe("[API] [Sales Portal] [Orders] [Update Order]", () => {
@@ -51,141 +52,7 @@ test.describe("[API] [Sales Portal] [Orders] [Update Order]", () => {
     if (productID) productID = "";
   });
 
-  test("Successful customer update", async ({ ordersApi, customerApiService }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    const lengthHistory = orderResponse.body.Order.history.length;
-
-    const newCustomer = await customerApiService.create(token, generateCustomerData());
-    extraCustomerIds.push(newCustomer._id);
-    orderData.customer = newCustomer._id;
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.OK,
-      IsSuccess: true,
-      ErrorMessage: null,
-      schema: createOrderSchema,
-    });
-
-    expect(response.body.Order.history.some((el) => el.customer === newCustomer._id)).toBe(true);
-    expect(response.body.Order.history.length).toBe(lengthHistory + 1);
-  });
-
-  test("Successful products update", async ({ ordersApi, productsApiService }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    const previousProducts = orderResponse.body.Order.products;
-    const previousTotalPrice = orderResponse.body.Order.total_price;
-
-    const newProduct1 = await productsApiService.create(token, generateProductData());
-    const newProduct2 = await productsApiService.create(token, generateProductData());
-    extraProductIds.push(newProduct1._id, newProduct2._id);
-    orderData.products = [newProduct1._id, newProduct2._id];
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.OK,
-      IsSuccess: true,
-      ErrorMessage: null,
-      schema: createOrderSchema,
-    });
-
-    expect(response.body.Order.products).not.toBe(previousProducts);
-    expect(response.body.Order.total_price).not.toBe(previousTotalPrice);
-  });
-
-  test("Update all fields simultaneously", async ({ ordersApi, productsApiService, customerApiService }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    const previousProducts = orderResponse.body.Order.products;
-    const previousTotalPrice = orderResponse.body.Order.total_price;
-    const lengthHistory = orderResponse.body.Order.history.length;
-
-    const newProduct1 = await productsApiService.create(token, generateProductData());
-    const newProduct2 = await productsApiService.create(token, generateProductData());
-    extraProductIds.push(newProduct1._id, newProduct2._id);
-    orderData.products = [newProduct1._id, newProduct2._id];
-
-    const newCustomer = await customerApiService.create(token, generateCustomerData());
-    extraCustomerIds.push(newCustomer._id);
-    orderData.customer = newCustomer._id;
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.OK,
-      IsSuccess: true,
-      ErrorMessage: null,
-      schema: createOrderSchema,
-    });
-
-    expect(response.body.Order.history.some((el) => el.customer === newCustomer._id)).toBe(true);
-    expect(response.body.Order.history.length).toBe(lengthHistory + 1);
-    expect(response.body.Order.products).not.toBe(previousProducts);
-    expect(response.body.Order.total_price).not.toBe(previousTotalPrice);
-  });
-
-  test("Missing required fields", async ({ ordersApi }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    orderData.products = [];
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.BAD_REQUEST,
-      IsSuccess: false,
-      ErrorMessage: null,
-      schema: errorSchema,
-    });
-  });
-
-  test("Attempt to update order with Received status", async ({ ordersApi }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-
-    const delivery = generateDeliveryData();
-    await ordersApi.updateDeliveryDetails(orderID, delivery, token);
-    await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.IN_PROCESS }, token);
-    await ordersApi.markProductsAsReceived(orderID, orderData.products, token);
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.BAD_REQUEST,
-      IsSuccess: false,
-      ErrorMessage: null,
-      schema: errorSchema,
-    });
-  });
-
-  test("Attempt to update order with Canceled status", async ({ ordersApi }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = orderResponse.body.Order._id;
-    await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.CANCELED }, token);
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.BAD_REQUEST,
-      IsSuccess: false,
-      ErrorMessage: null,
-      schema: errorSchema,
-    });
-  });
-
-  test("Non-existent order ID", async ({ ordersApi }) => {
-    const orderResponse = await ordersApi.create(orderData, token);
-    orderID = generateID();
-    await ordersApi.updateStatus({ id: orderID, status: ORDER_STATUSES.CANCELED }, token);
-    const response = await ordersApi.update(orderID, orderData, token);
-
-    validateResponse(response, {
-      status: STATUS_CODES.NOT_FOUND,
-      IsSuccess: false,
-      ErrorMessage: null,
-      schema: errorSchema,
-    });
-    orderID = orderResponse.body.Order._id;
-  });
-  test(
+  test.skip(
     "SC-078: Successful customer update",
     {
       tag: [TEST_TAG.REGRESSION, TEST_TAG.SMOKE, TEST_TAG.API, TEST_TAG.POSITIVE, COMPONENT_TAG.ORDERS],
