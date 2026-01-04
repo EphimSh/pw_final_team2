896cc775d2476ef9ac14620919d9fffd4341e228
EphimSh <ephimizm@gmail.com>
2026-01-04 14:36:04 +0300

    mute global-teardown

--- src/config/global.teardown.ts
+++ src/config/global.teardown.ts
@@ -1,11 +1,11 @@
-import { NotificationService } from "utils/notifications/notifications.service";
-import { TelegramService } from "utils/notifications/telegram.service";
+// import { NotificationService } from "utils/notifications/notifications.service";
+// import { TelegramService } from "utils/notifications/telegram.service";
 
-export default async function () {
-  if (!process.env.CI || process.env.TELEGRAM_NOTIFY_FROM_TEARDOWN !== "true") return;
-  if (!process.env.TELEGRAM_BOT_TOKEN || !process.env.TELEGRAM_CHAT_ID) return;
+// export default async function () {
+//   if (!process.env.CI) return;
+//   if (!process.env.TELEGRAM_BOT_TOKEN || !process.env.TELEGRAM_CHAT_ID) return;
 
-  const notificationService = new NotificationService(new TelegramService());
+//   const notificationService = new NotificationService(new TelegramService());
 
-  await notificationService.postNotification(`Test run finished!`);
-}
+//   await notificationService.postNotification(`Test run finished!`);
+// }
