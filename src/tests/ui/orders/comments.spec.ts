7b32d5cacaaac0ecf18c3e4850c0ab8b28be61a7
EphimSh <ephimizm@gmail.com>
2026-01-04 12:38:21 +0300

    add .deleteOrderWithCustomerAndProduct

--- src/tests/ui/orders/comments.spec.ts
+++ src/tests/ui/orders/comments.spec.ts
@@ -4,23 +4,15 @@ import { test } from "fixtures";
 test.describe("[UI] [Sales Portal] [Orders] [Order Details]", () => {
   let token = "";
   let orderId = "";
-  let customerId = "";
-  let productIds: string[] = [];
 
   test.beforeAll(async ({ loginApiService, ordersApiService }) => {
     token = await loginApiService.loginAsAdmin();
     const order = await ordersApiService.createDraftOrder(token);
     orderId = order._id;
-    customerId = order.customer._id;
-    productIds = order.products.map((product) => product._id);
   });
 
-  test.afterAll(async ({ ordersApiService, customerApiService, productsApiService }) => {
-    if (orderId) await ordersApiService.deleteOrder(orderId, token);
-    for (const productId of productIds) {
-      await productsApiService.delete(token, productId);
-    }
-    if (customerId) await customerApiService.delete(token, customerId);
+  test.afterAll(async ({ ordersApiService }) => {
+    if (orderId) await ordersApiService.deleteOrderWithCustomerAndProduct(orderId, token);
   });
 
   test("Add comment to Order", async ({ orderUIService, loginUIService }) => {
