300cd1f2c85c5dc6d0867bc26530eb14d3551c1b
EphimSh <ephimizm@gmail.com>
2026-01-04 14:25:10 +0300

    fix reporting issues

--- src/config/global.teardown.ts
+++ src/config/global.teardown.ts
@@ -2,14 +2,10 @@ import { NotificationService } from "utils/notifications/notifications.service";
 import { TelegramService } from "utils/notifications/telegram.service";
 
 export default async function () {
-  if (!process.env.CI) return;
+  if (!process.env.CI || process.env.TELEGRAM_NOTIFY_FROM_TEARDOWN !== "true") return;
+  if (!process.env.TELEGRAM_BOT_TOKEN || !process.env.TELEGRAM_CHAT_ID) return;
 
   const notificationService = new NotificationService(new TelegramService());
 
-  await notificationService.postNotification(`Test run finished!
-
-  Link to deployed report:
-
-  //FIXME: update report link if needed
-  https://EphimSh.github.io/pw_final_team2/allure-report/#`);
+  await notificationService.postNotification(`Test run finished!`);
 }
